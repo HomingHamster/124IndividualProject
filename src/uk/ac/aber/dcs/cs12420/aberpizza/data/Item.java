d08476043d34d1b3d5126987a10ab30ce97cfd63
Felix Farquharson <fef@aber.ac.uk>
2012-03-26 23:33:54 +0100

    *added packages *added main class *added some of design we had already

---
+++ src/uk/ac/aber/dcs/cs12420/aberpizza/data/Item.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package uk.ac.aber.dcs.cs12420.aberpizza.data;
+
+/**
+ * @author fef
+ *
+ */
+public interface Item {
+
+}
---
+++ src/uk/ac/aber/dcs/cs12420/aberpizza/gui/Manager.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package uk.ac.aber.dcs.cs12420.aberpizza.gui;
+
+/**
+ * @author fef
+ *
+ */
+public class Manager {
+
+}
