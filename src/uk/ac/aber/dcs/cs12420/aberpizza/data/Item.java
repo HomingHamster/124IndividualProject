61dce1ab1c6b43e4691697a57c740e06ef7ef9d9
Felix Farquharson <fef@aber.ac.uk>
2012-03-27 00:23:46 +0100

    changes to Item

--- src/uk/ac/aber/dcs/cs12420/aberpizza/data/Item.java
+++ src/uk/ac/aber/dcs/cs12420/aberpizza/data/Item.java
@@ -3,10 +3,22 @@
  */
 package uk.ac.aber.dcs.cs12420.aberpizza.data;
 
+import java.math.BigDecimal;
+
 /**
  * @author fef
  *
  */
 public interface Item {
+	public BigDecimal getPrice();
+	public void setPrice(BigDecimal price);
 
+	public String getDecription();
+	public void setDescription(String description);
+
+	public String getName();
+	public void setName(String name);
+
+	public ItemType getItemType();
+	public void setItemType(ItemType type);
 }
